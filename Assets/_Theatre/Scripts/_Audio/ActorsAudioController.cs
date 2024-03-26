6171d42b1f452587038a41eaf2d3aea18c6d109c
Dvvan696 <D.vvaann@gmail.com>
2024-03-26 23:05:56 +0300

    4 story

--- Assets/_Theatre/Scripts/_Audio/ActorsAudioController.cs
+++ Assets/_Theatre/Scripts/_Audio/ActorsAudioController.cs
@@ -42,6 +42,10 @@ public class ActorsAudioController : MonoBehaviour
     {
         actors[6].NextSound();
     }
+    public void SixthBlind()
+    {
+        actors[7].NextSound();
+    }
 
     public void NextBlock()
     {
